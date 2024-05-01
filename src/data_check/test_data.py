660e946ae3bc2a36231ab7e3603b39b2ca7a1b7a
cherfongfoo <mailcherfong@gmail.com>
2024-05-01 17:13:18 +0800

    fit minor bug in data check

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -63,10 +63,10 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
 ########################################################
 # Implement here test_row_count and test_price_range   #
 ########################################################
-def test_row_count(data: pd.Dataframe):
+def test_row_count(data: pd.DataFrame):
     assert 15000 < data.shape[0] < 1000000
 
-def test_price_range(data: pd.Dataframe, min_price:float, max_price:float):
+def test_price_range(data: pd.DataFrame, min_price:float, max_price:float):
     idx = data.price.between(min_price, max_price)
     data_within_range = data[idx].copy()
     assert len(data_within_range) == len(data)
